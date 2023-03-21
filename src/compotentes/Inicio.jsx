901de009a123e8ea3417d4e203b8d77a026b11ec
hectorfl10 <windowshector@gmail.com>
2023-03-21 13:35:02 -0600

    consumo de api con estilos

--- src/compotentes/Inicio.jsx
+++ src/compotentes/Inicio.jsx
@@ -1,27 +1,46 @@
-import React,{useState, useEffect} from 'react'
-import {todosPersonajes} from '../funciones/funciones'
+import React, {useState, useEffect} from 'react';
+import {todosPersonajes} from '../funciones/funciones';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Inicio = () => {
-  const [personajes, setPersonajes] =useState(null)
-  useEffect(()=>{
-    todosPersonajes(setPersonajes)
-  },[])
+  const [personajes, setPersonajes] = useState(null);
+
+  useEffect(() => {
+    todosPersonajes(setPersonajes);
+  }, []);
+
   return (
-    <>
-      {personajes != null ? (
-        personajes.map(personaje =>(
-          <div>key={personaje.id}
-          <a href={`/personaje/${personaje.id}`}>{personaje.name}</a>
-          <img src={personaje.image}alt=""/>
+    <div className="container">
+    <div className="text-center">
+      <h1>Personajes Rick y Morty</h1>
+    </div>
+      <div className="row">
+        {personajes != null ? (
+          personajes.map(personaje => (
+            <div className="col-3 mb-3" key={personaje.id}>
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={personaje.image}
+                  alt={`Imagen de ${personaje.name}`}
+                />
+                <div className="card-body text-center">
+                  <h5 className="card-title">{personaje.name}</h5>
+                  <a href={`/personaje/${personaje.id}`} className="btn btn-primary">
+                    Ver detalles
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="text-center">
+            <p>No hay personajes</p>
           </div>
-        ))
-      ):('no hay personjaes')}
+        )}
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-    </>
-  )
-}
-
-export default Inicio
+export default Inicio;
--- src/compotentes/Personaje.jsx
+++ src/compotentes/Personaje.jsx
@@ -1,6 +1,9 @@
-import React , {useEffect, useState}from 'react'
+import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import {unicopersonaje} from '../funciones/funciones'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Personaje = () => {
   const [personaje, setPersonaje] = useState(null)
 
@@ -8,17 +11,23 @@ const Personaje = () => {
   useEffect(()=>{
     unicopersonaje(params.id, setPersonaje)
   },[])
+  
   return (
     <>
- {personaje != null ?(
-  <div>
-      <h2>personaje con el id {params.id}</h2> 
-      <p>con el nombre:{personaje.name}</p>
-      <img src={personaje.image}a lt=''/>
-  </div>
- ):('no hay personaje')}
+      {personaje != null ? (
+        <div className="container">
+          <h2 className="my-4 text-center text-danger">Personaje con el id {params.id}</h2> 
+          <div className="d-flex justify-content-center">
+            <img src={personaje.image} alt="" className="img-fluid" />
+          </div>
+          <p className="my-4 text-center ">Con el nombre: {personaje.name}</p>
+        </div>
+      ) : (
+        <div className="text-center">
+          <p>No hay personaje</p>
+        </div>
+      )}
     </>
-
   )
 }
 
